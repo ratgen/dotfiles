b27e2d4ef05100b34d751afe364db13b3ce7863d
Peter Heilbo Ratgen <peter@pratgen.dk>
2020-03-31 23:42:04 +0200

    add setup, init and bashrc

---
+++ setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+mkdir -p $HOME/.config/nvim/
+
+dotFileDir=$(pwd)
+
+function dotfilelink {
+  dest="$HOME/$2"
+  
+  if [ -h $dest ]; then
+    echo "Removing existing symlink"
+    rm $dest
+  fi
+
+  echo "Creating symlink"
+  ln -s $dotFileDir/$1 $dest
+}
+
+dotfilelink bashrc  .bashrc
+dotfilelink init.vim .config/nvim/init.vim
+
+if [ -f /etc/os-release ]; then
+  . /etc/os-release
+  OS=$ID_LIKE
+else
+  OS=$(uname -r)
+fi
+
+if [ $OS=="arch" ]; then
+  sudo pacman -Syu
+  sudo pacman -S --needed neovim
+elif [$OS=="debian" ]; then
+  sudo apt install neovim
+else
+  echo "no installtion for this setup file"
+fi
+
+nvim +PlugInstall +q! +q! +q!
