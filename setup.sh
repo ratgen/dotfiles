cac0504b1dd443a3bd1af1a1fc3b5fe524c02e73
Peter Heilbo Ratgen <peter@pratgen.dk>
2020-10-02 22:16:04 +0200

    relink

--- setup.sh
+++ setup.sh
@@ -1,58 +1,51 @@
 #!/bin/bash
+mkdir -p $HOME/.config/nvim/
 dotFileDir=$(pwd)
+unameS=$(uname -s)
+
 function dotfilelink {
   dest="$HOME/$2"
   if [ -h $dest ]; then
     echo "Removing existing symlink"
     rm $dest
   fi
-
   echo "Creating symlink"
   ln -s $dotFileDir/$1 $dest
 }
-unameS=$(uname -s)
 
-if [ $1  == "relink" ]; then
+function linkAllFiles {
   dotfilelink init.vim .config/nvim/init.vim
   dotfilelink gitconfig .gitconfig
   dotfilelink coc-settings.json .config/nvim/coc-settings.json
   if [ $unameS = "Linux" ]; then
     dotfilelink bashrc .bashrc
     source ~/.bashrc
-    exit
   elif [ $unameS = "Darwin" ]; then
     dotfilelink bashrc .bash_profile
     source ~/.bash_profile
-    exit
   else
     echo "not a supported system"
   fi 
+}
+
+if [ "$1" = "relink" ]; then
+  linkAllFiles
+  exit
 fi
 
-mkdir -p $HOME/.config/nvim/
-
-
-dotfilelink init.vim .config/nvim/init.vim
-dotfilelink gitconfig .gitconfig
-dotfilelink coc-settings.json .config/nvim/coc-settings.json
+linkAllFiles 
 
 packages="neovim nodejs npm neofetch fzf"
-
 if [ $unameS = "Linux" ]; then
-  dotfilelink bashrc .bashrc
   if test -f "/etc/arch-release" ; then
     sudo pacman -S --needed $packages fortune-mod
   elif test -f "/etc/debian_version" ; then
     sudo apt install $packages fortune
   else
     echo "no supported package manager"
-    source ~/.bashrc
   fi
 elif [ $unameS = "Darwin" ]; then
-  dotfilelink bashrc .bash_profile
   brew install $packages fortune
-  echo "done brewing"
-  source ~/.bash_profile
 else
   echo "not a supported system"
 fi
