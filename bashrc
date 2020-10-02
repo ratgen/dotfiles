315a320b9ab9357af17f82631b9c80aeb6ed8309
Peter Heilbo Ratgen <peter@pratgen.dk>
2020-10-02 13:43:56 +0200

    mac adaptations

--- bashrc
+++ bashrc
@@ -3,25 +3,29 @@ PS1='[\D{%d/%m %R} \u\e[01;32m@\h: \e[1;34m\w\e[00m] $ '
 
 #useful aliases
 #list all with human size
-alias ls='ls --color=auto'
 alias la='ls -lah'
 #list folders size
 alias list='du -cbsh *'
 alias python=python3
 
+unameS=$(uname -s)
 # fzf 
 ## bindings and fzf completions 
-source /usr/share/fzf/key-bindings.bash
-source /usr/share/fzf/completion.bash
+if [ $unameS = "Linux" ]; then
+  source /usr/share/fzf/key-bindings.bash
+  source /usr/share/fzf/completion.bash
+elif [ $unameS = "Darwin" ]; then
+  source /usr/local/Cellar/fzf/0.22.0/shell/key-bindings.bash
+  source /usr/local/Cellar/fzf/0.22.0/shell/completion.bash
+else
+  echo "not a supported system"
+fi
 
 ## 
 if type rg &> /dev/null; then
   export FZF_DEFAULT_COMMAND='rg --files'
   export FZF_DEFAULT_OPTS='-m --height 50% --border'
 fi
-
-# use ripgrep for FZF
-export FZF_DEFAULT_COMMAND='rg'
 
 op(){
   xdg-open "$1"
--- setup.sh
+++ setup.sh
@@ -5,7 +5,6 @@ dotFileDir=$(pwd)
 
 function dotfilelink {
   dest="$HOME/$2"
- 
   if [ -h $dest ]; then
     echo "Removing existing symlink"
     rm $dest
@@ -15,7 +14,6 @@ function dotfilelink {
   ln -s $dotFileDir/$1 $dest
 }
 
-dotfilelink bashrc  .bashrc
 dotfilelink init.vim .config/nvim/init.vim
 dotfilelink gitconfig .gitconfig
 dotfilelink coc-settings.json .config/nvim/coc-settings.json
@@ -25,16 +23,20 @@ declare packageManager
 packages="neovim nodejs npm neofetch fzf"
 
 if [ $unameS = "Linux" ]; then
+  dotfilelink bashrc .bashrc
   if test -f "/etc/arch-release" ; then
     sudo pacman -S --needed $packages fortune-mod
   elif test -f "/etc/debian_version" ; then
     sudo apt install $packages fortune
   else
     echo "no supported package manager"
+  source ~/.bashrc
   fi
 elif [ $unameS = "Darwin" ]; then
+  dotfilelink bashrc .bash_profile
   brew install $packages fortune
   echo "done brewing"
+  source ~/.bash_profile
 else
   echo "not a supported system"
 fi
@@ -42,4 +44,3 @@ fi
 nvim +PlugInstall +q! +q! +q!
 sudo npm i -g bash-language-server
 
-source ~/.bashrc
