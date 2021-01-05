545e4501ce77a5cd3d910ae0e7166006f7b2b71e
Peter Heilbo Ratgen <peter@pratgen.dk>
2021-01-05 12:21:38 +0100

    updates to script

--- mac-polybar/scroll_lights.sh
+++ mac-polybar/scroll_lights.sh
@@ -3,6 +3,5 @@
 zscroll -l 30 \
   --delay 0.1 \
   --scroll 1  \
-  --update-check true "python3 $HOME/.config/polybar/lights.py -K XPtyaMjGD7f6Liuh 192.168.1.67"  
-
+  --update-check true "python3 $HOME/.config/polybar/lights.py -K XPtyaMjGD7f6Liuh 192.168.1.67" &
 wait
