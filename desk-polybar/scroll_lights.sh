8f6f7875bac12a9dd7b3062947465ea85166bf80
Peter Heilbo Ratgen <peter@pratgen.dk>
2021-01-06 14:29:33 +0100

    desk

--- desk-polybar/scroll_lights.sh
+++ desk-polybar/scroll_lights.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 zscroll -l 45 \
-  --delay 0.008 \
+  --delay 0.2 \
   --scroll-padding " | " \
   --update-check true "python $HOME/.config/polybar/lights.py -K XPtyaMjGD7f6Liuh 192.168.1.67" &
 wait
