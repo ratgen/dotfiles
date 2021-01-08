e4e488698ee4cd2647479697d914b393ec2b7485
Peter Heilbo Ratgen <peter@pratgen.dk>
2021-01-08 14:24:56 +0100

    add polybar

--- desk-polybar/scroll_lights.sh
+++ desk-polybar/scroll_lights.sh
@@ -4,4 +4,5 @@ zscroll --length=30 \
   --delay 0.2 \
   --scroll-padding " | " \
   --update-check true "python $HOME/.config/polybar/lights.py -K XPtyaMjGD7f6Liuh 192.168.1.67" &
+  --update-interval 15
 wait
