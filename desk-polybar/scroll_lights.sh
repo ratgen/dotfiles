9738f9d2868e36e3f53c230f28ff7e6743b95aff
Peter Heilbo Ratgen <peter@pratgen.dk>
2021-01-13 14:37:57 +0100

    playerctl

--- desk-polybar/scroll_lights.sh
+++ desk-polybar/scroll_lights.sh
@@ -3,6 +3,6 @@
 zscroll --length=30 \
   --delay 0.2 \
   --scroll-padding " | " \
-  --update-check true "python $HOME/.config/polybar/lights.py -K XPtyaMjGD7f6Liuh 192.168.1.67" &
-  --update-interval 15
+  --update-check true "python $HOME/.config/polybar/lights.py -K XPtyaMjGD7f6Liuh 192.168.1.67" \
+  -U 15 & 
 wait
