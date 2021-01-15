7ac461a87085f87dc1a4f1c4da3a115ec52e8034
Peter Heilbo Ratgen <peter@pratgen.dk>
2021-01-15 12:13:20 +0100

    move to symbolic links

--- mac-polybar/scroll_lights.sh
+++ mac-polybar/scroll_lights.sh
@@ -1,8 +1,1 @@
-#!/bin/bash
-
-zscroll --length=30 \
-  --delay 0.2 \
-  --scroll-padding " | " \
-  --update-check true "python $HOME/.config/polybar/lights.py -K XPtyaMjGD7f6Liuh 192.168.1.67" \
-  -U 15 & 
-wait
+../desk-polybar/scroll_lights.sh
