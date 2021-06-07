fa83ee6d067c5e015660598a8d13df361d68805c
Peter Heilbo Ratgen <peter@pratgen.dk>
2021-06-07 13:46:46 +0200

    feat(screenlayout): Add screenlayout setup
    Add screenlayout setup.

---
+++ screenlayout/screen.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+xrandr \
+--output DP-4.8  --pos 0x0 --rotate normal \
+--output DP-2 --primary --mode 2560x1440 --pos 2560x0 --rotate normal \
+--output DP-0.8 --pos 5120x0 --rotate normal \
+--output DVI-D-0 --off \
+--output HDMI-0 --off \
+--output DP-0 --off \
+--output DP-1 --off \
+--output DP-2 --off \
+--output DP-3 --off \
+--output DP-5 --off \
+--output DVI-D-1-0 --off \
+--output HDMI-1-0 --off \
+--output DP-1-0 --off \
+--output DP-1-1 --off \
+--output DP-1-2 --off \
+--output DP-1-3 --off \
+--output DP-1-4 --off \
+--output DP-1-5 --off
+
+#--mode 2560x1440_59.95
