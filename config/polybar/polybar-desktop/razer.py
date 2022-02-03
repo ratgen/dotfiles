fba76742846a142dfbdd5d14ba2346b0c9e2f152
Peter Heilbo Ratgen <peter@pratgen.dk>
2022-02-03 21:39:10 +0100

    Reinstalled openrazer

--- config/polybar/polybar-desktop/razer.py
+++ config/polybar/polybar-desktop/razer.py
@@ -1,26 +1,25 @@
-from openrazer import DeviceManager
+from openrazer.client import DeviceManager
 
 device_manager = DeviceManager()
 
-mamba = None
 dev = []
 
 for device in device_manager.devices:
     mamba = device
     dev.append(device)
 
-level = mamba.battery_level
+    level = mamba.battery_level
 
-if mamba.is_charging:
-    print(" " + str(mamba.battery_level) + "%")
-else:
-    if level <= 10:
-        print("  " + str(mamba.battery_level) + "%")
-    elif  level > 10 and level <= 35:
-        print("  " + str(mamba.battery_level) + "%")
-    elif level > 35 and level <= 65:
-        print("  " + str(mamba.battery_level) + "%")
-    elif level > 65 and level <= 90:
-        print("  " + str(mamba.battery_level) + "%")
-    if level > 90:
-        print("  " + str(mamba.battery_level) + "%")
+    if mamba.is_charging:
+        print(" " + str(mamba.battery_level) + "%")
+    else:
+        if level <= 10:
+            print("  " + str(mamba.battery_level) + "%")
+        elif  level > 10 and level <= 35:
+            print("  " + str(mamba.battery_level) + "%")
+        elif level > 35 and level <= 65:
+            print("  " + str(mamba.battery_level) + "%")
+        elif level > 65 and level <= 90:
+            print("  " + str(mamba.battery_level) + "%")
+        if level > 90:
+            print("  " + str(mamba.battery_level) + "%")
