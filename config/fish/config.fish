0f31c423da3b7a22d52597d15364dd87273de5fc
Peter Heilbo Ratgen <peter@pratgen.dk>
2022-04-30 14:21:39 +0200

    Add fish configuration in its entirety

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -11,10 +11,6 @@ if status is-interactive
 
   set -xU LS_COLORS 'di=36:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43'
 
-  alias neofetch "neofetch --color_blocks off"
-  alias gitrmignore "git ls-files -i -c --exclude-from=.gitignore | xargs git rm --cached"
-  alias zathura "zathura --fork"
-
   neofetch
 end
 
