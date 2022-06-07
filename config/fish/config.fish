865f62ff539b47e110f5d5b9f8eca2456c0cd9a9
Peter Heilbo Ratgen <peter@pratgen.dk>
2022-06-07 17:10:51 +0200

    android sdk

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -9,6 +9,10 @@ if status is-interactive
 
   set fish_greeting
 
+  set -x ANDROID_SDK_ROOT $HOME/Android/Sdk
+  set -x PATH $PATH:$ANDROID_SDK_ROOT/emulator
+  set -x PATH $PATH:$ANDROID_SDK_ROOT/platform-tools
+
   set -xU LS_COLORS 'di=36:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43'
 
   neofetch
