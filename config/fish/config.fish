03a146229cf28299a9aaafc3db1637713e75af72
Peter Heilbo Ratgen <peter@pratgen.dk>
2022-06-08 21:30:28 +0200

    updatees

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -16,6 +16,8 @@ if status is-interactive
 
   set -xU LS_COLORS 'di=36:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43'
 
+  alias screenshot "scrot -s /home/peter/screenshots/%y-%m-%d_%H:%M:%S.png"
+
   alias zathura "zathura --fork"
 
   alias fm "vifm $(pwd)"
