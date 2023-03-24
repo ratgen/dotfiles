bc4918a747d6c6dff9028e70b06302cd5c675102
Peter Heilbo Ratgen <peter@pratgen.dk>
2023-03-24 16:11:21 +0100

    Remove airline

--- vim/after/plugin/vim-airline.lua
+++
@@ -1,3 +0,0 @@
-vim.cmd("let g:airline#extensions#tabline#enabled = 1")
-vim.cmd("let g:airline#extensions#tabline#buffer_idx_mode = 1")
-vim.cmd("let g:airline_powerline_fonts = 1")
--- vim/lua/editor_config/init.lua
+++ vim/lua/editor_config/init.lua
@@ -15,4 +15,14 @@ set.confirm = true
 set.cursorline = true
 set.mouse = nil
 
+set.scrolloff = 10
+set.signcolumn = "yes"
+
 set.textwidth = 80
+set.termguicolors = true
+set.updatetime = 50
+
+set.modeline = true
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
