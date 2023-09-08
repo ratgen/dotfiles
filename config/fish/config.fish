daaa397ab56f6da68ba20e3cfc5462376eb682e2
Peter Heilbo Ratgen <peter@pratgen.dk>
2023-09-08 10:21:39 +0200

    small updates

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -23,9 +23,7 @@ if status is-interactive
 
   alias zathura "zathura --fork"
   alias fm "vifm $(pwd)"
-  alias ls "ls --hyperlink=auto --color=auto"
-
-  alias s "kitty +kitten ssh"
+  alias ls "ls --color=auto"
 
   neofetch
 end
