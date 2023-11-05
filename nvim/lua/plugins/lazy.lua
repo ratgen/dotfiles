3d0cff1001bfc8f6684eac79545357278724b59e
Peter Heilbo Ratgen <peter@pratgen.dk>
2023-11-05 22:25:53 +0100

    Move to the lazy.nvim plugin manager

---
+++ nvim/lua/plugins/lazy.lua
@@ -0,0 +1,98 @@
+vim.g.mapleader = ' '
+
+require('lazy').setup({
+  'wbthomason/packer.nvim',
+  {
+    'nvim-telescope/telescope.nvim',
+    -- or                            , branch = '0.1.x',
+    dependencies = { { 'nvim-lua/plenary.nvim' } }
+  },
+  {
+    'folke/trouble.nvim',
+    dependencies = { { 'nvim-tree/nvim-web-devicons' } }
+  },
+
+  { "nvim-telescope/telescope-bibtex.nvim" },
+
+  'folke/tokyonight.nvim',
+  "nvim-treesitter/nvim-treesitter",
+
+  'lervag/vimtex',
+  'junegunn/vim-easy-align',
+
+  -- Surrounding for quoting and parenthesizing
+  'machakann/vim-sandwich',
+
+  -- Git commands inside vim
+  'tpope/vim-fugitive',
+  'SirVer/ultisnips',
+  'honza/vim-snippets',
+
+  'williamboman/mason-lspconfig.nvim',
+  'williamboman/mason.nvim',
+  'neovim/nvim-lspconfig',
+
+  -- Autocompletion plugin
+  'hrsh7th/nvim-cmp',
+
+  --  sources for cmp-nvim
+  'quangnguyen30192/cmp-nvim-ultisnips',
+  'hrsh7th/cmp-nvim-lsp',
+  'hrsh7th/cmp-path',
+  'hrsh7th/cmp-calc',
+  'hrsh7th/cmp-buffer',
+  'hrsh7th/cmp-cmdline',
+  'nvim-lua/plenary.nvim',
+  'petertriho/cmp-git',
+  'micangl/cmp-vimtex',
+  --   'lervag/cmp-vimtex' causes bad things
+
+  'windwp/nvim-autopairs',
+
+  'Yggdroot/indentLine',
+  'wellle/targets.vim',
+  'yuezk/vim-js',
+  'maxmellon/vim-jsx-pretty',
+
+  'aklt/plantuml-syntax',
+  'tyru/open-browser.vim',
+  'weirongxu/plantuml-previewer.vim',
+
+  'nvim-tree/nvim-web-devicons',
+  {
+    "zbirenbaum/copilot.lua",
+    config = function()
+      require("copilot").setup({
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+      })
+    end,
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    config = function()
+      require("copilot_cmp").setup()
+    end
+  },
+  'onsails/lspkind.nvim',
+
+  {
+    "iamcco/markdown-preview.nvim",
+      cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+      ft = { "markdown" },
+      build = function() vim.fn["mkdp#util#install"]() end,
+  },
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons', opt = true }
+  },
+
+  {
+    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+    config = function()
+      require("lsp_lines").setup()
+    end,
+  },
+
+  'nvimtools/none-ls.nvim',
+})
