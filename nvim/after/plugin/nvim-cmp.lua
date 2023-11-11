9c8bc0585f32c308adcad8bc8a61ac09a1280d7e
Peter Heilbo Ratgen <peter@pratgen.dk>
2023-11-11 13:35:44 +0100

    Tmux confi

--- nvim/after/plugin/nvim-cmp.lua
+++ nvim/after/plugin/nvim-cmp.lua
@@ -1,5 +1,4 @@
-
--- vim.g.UltiSnipsExpandTrigger = '<Plug>(ultisnips_expand)'      
+-- vim.g.UltiSnipsExpandTrigger = '<Plug>(ultisnips_expand)'
 vim.g.UltiSnipsJumpForwardTrigger = '<Plug>(ultisnips_jump_forward)'
 vim.g.UltiSnipsJumpBackwardTrigger = '<Plug>(ultisnips_jump_backward)'
 vim.g.UltiSnipsListSnippets = '<c-x><c-s>'
@@ -9,7 +8,7 @@ local t = function(str)
   return vim.api.nvim_replace_termcodes(str, true, true, true)
 end
 
-local cmp = require'cmp'
+local cmp = require 'cmp'
 local lspkind = require('lspkind')
 cmp.setup({
   completion = {
@@ -17,13 +16,13 @@ cmp.setup({
     keyword_pattern = [[\%(-\?\d\+\%(\.\d\+\)\?\|\h\w*\%(-\w*\)*\)]],
     keyword_length = 1,
     get_trigger_characters = function(trigger_characters)
-    return trigger_characters
-  end,
+      return trigger_characters
+    end,
   },
   snippet = {
-      expand = function(args)
+    expand = function(args)
       vim.fn["UltiSnips#Anon"](args.body)
-  end,
+    end,
   },
   preselect = cmp.PreselectMode.Item,
 
@@ -32,102 +31,102 @@ cmp.setup({
     documentation = cmp.config.window.bordered(),
   },
   confirmation = {
-      default_behavior = cmp.ConfirmBehavior.Insert,
-      get_commit_characters = function(commit_characters)
+    default_behavior = cmp.ConfirmBehavior.Insert,
+    get_commit_characters = function(commit_characters)
       return commit_characters
     end,
   },
   event = {},
   mapping = {
-        ["<Tab>"] = cmp.mapping({
-            c = function()
-                if cmp.visible() then
-                  cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
-                else
-                    cmp.complete()
-                end
-            end,
-            i = function(fallback)
-                if cmp.visible() then
-                    cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
-                elseif vim.fn["UltiSnips#CanJumpForwards"]() == 1 then
-                    vim.api.nvim_feedkeys(t("<Plug>(ultisnips_jump_forward)"), 'm', true)
-                else
-                    fallback()
-                end
-            end,
-            s = function(fallback)
-                if vim.fn["UltiSnips#CanJumpForwards"]() == 1 then
-                    vim.api.nvim_feedkeys(t("<Plug>(ultisnips_jump_forward)"), 'm', true)
-                else
-                    fallback()
-                end
-            end
-        }),
-        ["<S-Tab>"] = cmp.mapping({
-            c = function()
-                if cmp.visible() then
-                    cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
-                else
-                    cmp.complete()
-                end
-            end,
-            i = function(fallback)
-                if cmp.visible() then
-                    cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
-                elseif vim.fn["UltiSnips#CanJumpBackwards"]() == 1 then
-                    return vim.api.nvim_feedkeys( t("<Plug>(ultisnips_jump_backward)"), 'm', true)
-                else
-                    fallback()
-                end
-            end,
-            s = function(fallback)
-                if vim.fn["UltiSnips#CanJumpBackwards"]() == 1 then
-                    return vim.api.nvim_feedkeys( t("<Plug>(ultisnips_jump_backward)"), 'm', true)
-                else
-                    fallback()
-                end
-            end
-        }),
-        ['<Down>'] = cmp.mapping(cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }), {'i'}),
-        ['<Up>'] = cmp.mapping(cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }), {'i'}),
-        ['<C-n>'] = cmp.mapping({
-            c = function()
-                if cmp.visible() then
-                    cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-                else
-                    vim.api.nvim_feedkeys(t('<Down>'), 'n', true)
-                end
-            end,
-            i = function(fallback)
-                if cmp.visible() then
-                    cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-                else
-                    fallback()
-                end
-            end
-        }),
-        ['<C-p>'] = cmp.mapping({
-            c = function()
-                if cmp.visible() then
-                    cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
-                else
-                    vim.api.nvim_feedkeys(t('<Up>'), 'n', true)
-                end
-            end,
-            i = function(fallback)
-                if cmp.visible() then
-                    cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
-                else
-                    fallback()
-                end
-            end
-        }),
-        ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), {'i', 'c'}),
-        ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), {'i', 'c'}),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping({ i = cmp.mapping.close(), c = cmp.mapping.close() }),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ["<Tab>"] = cmp.mapping({
+      c = function()
+        if cmp.visible() then
+          cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
+        else
+          cmp.complete()
+        end
+      end,
+      i = function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
+        elseif vim.fn["UltiSnips#CanJumpForwards"]() == 1 then
+          vim.api.nvim_feedkeys(t("<Plug>(ultisnips_jump_forward)"), 'm', true)
+        else
+          fallback()
+        end
+      end,
+      s = function(fallback)
+        if vim.fn["UltiSnips#CanJumpForwards"]() == 1 then
+          vim.api.nvim_feedkeys(t("<Plug>(ultisnips_jump_forward)"), 'm', true)
+        else
+          fallback()
+        end
+      end
+    }),
+    ["<S-Tab>"] = cmp.mapping({
+      c = function()
+        if cmp.visible() then
+          cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
+        else
+          cmp.complete()
+        end
+      end,
+      i = function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
+        elseif vim.fn["UltiSnips#CanJumpBackwards"]() == 1 then
+          return vim.api.nvim_feedkeys(t("<Plug>(ultisnips_jump_backward)"), 'm', true)
+        else
+          fallback()
+        end
+      end,
+      s = function(fallback)
+        if vim.fn["UltiSnips#CanJumpBackwards"]() == 1 then
+          return vim.api.nvim_feedkeys(t("<Plug>(ultisnips_jump_backward)"), 'm', true)
+        else
+          fallback()
+        end
+      end
+    }),
+    ['<Down>'] = cmp.mapping(cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }), { 'i' }),
+    ['<Up>'] = cmp.mapping(cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }), { 'i' }),
+    ['<C-n>'] = cmp.mapping({
+      c = function()
+        if cmp.visible() then
+          cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+        else
+          vim.api.nvim_feedkeys(t('<Down>'), 'n', true)
+        end
+      end,
+      i = function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+        else
+          fallback()
+        end
+      end
+    }),
+    ['<C-p>'] = cmp.mapping({
+      c = function()
+        if cmp.visible() then
+          cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+        else
+          vim.api.nvim_feedkeys(t('<Up>'), 'n', true)
+        end
+      end,
+      i = function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+        else
+          fallback()
+        end
+      end
+    }),
+    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping({ i = cmp.mapping.close(), c = cmp.mapping.close() }),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 
   },
   formatting = {
@@ -137,15 +136,15 @@ cmp.setup({
     ghost_text = false,
   },
   sources = {
-      { name = "nvim_lsp", group_index = 1 },
-      -- { name = 'vimtex', },
-      { name = "copilot", group_index = 1 },
-      { name = "path", group_index = 2},
-      -- { name = "buffer" },
-      { name = "calc", group_index = 2 },
-      { name = "ultisnips", group_index = 2 },
-    }
+    { name = "nvim_lsp",  group_index = 1 },
+    -- { name = 'vimtex', },
+    { name = "copilot",   group_index = 1 },
+    { name = "path",      group_index = 2 },
+    -- { name = "buffer" },
+    { name = "calc",      group_index = 2 },
+    { name = "ultisnips", group_index = 2 },
   }
+}
 )
 
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
@@ -166,16 +165,33 @@ require("mason-lspconfig").setup_handlers {
   -- The first entry (without a key) will be the default handler
   -- and will be called for each installed server that doesn't have
   -- a dedicated handler.
-  function (server_name) -- default handler (optional)
+  function(server_name) -- default handler (optional)
     require("lspconfig")[server_name].setup {
-        capabilities = capabilities,
+      capabilities = capabilities,
     }
   end,
   -- Next, you can provide a dedicated handler for specific servers.
   -- For example, a handler override for the `rust_analyzer`:
 }
 
-require'lspconfig'.lua_ls.setup {
+require 'lspconfig'.pylsp.setup {
+  capabilities = capabilities,
+  settings = {
+    pylsp = {
+      plugins = {
+        jedi_completion = {
+          enabled = true,
+          include_params = true,
+          fuzzy = true,
+          cache_for = {"pandas", "numpy", "tensorflow", "matplotlib", "os", "json", "skimage", "torch"},
+          include_function_objects = true
+        },
+      }
+    }
+  }
+}
+
+require 'lspconfig'.lua_ls.setup {
   settings = {
     Lua = {
       runtime = {
--- nvim/ftplugin/python.lua
+++ nvim/ftplugin/python.lua
@@ -1,7 +1,15 @@
 local null_ls = require("null-ls")
 null_ls.setup({
   sources = {
-      null_ls.builtins.diagnostics.ruff,
+--      null_ls.builtins.diagnostics.ruff,
       null_ls.builtins.code_actions.refactoring
   },
 })
+local cmp = require('cmp')
+local sources = cmp.get_config().sources
+for i = #sources, 1, -1 do
+  if sources[i].name == 'copilot' then
+    table.remove(sources, i)
+  end
+end
+cmp.setup.buffer({ sources = sources })
