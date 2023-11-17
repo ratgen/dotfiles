f5a4cd69ba9949effde6ef08647e7e402b3e80d3
Peter Heilbo Ratgen <peter@pratgen.dk>
2023-11-17 15:11:02 +0100

    Add dap to confiAdd dap to configg

--- nvim/lua/editor_config/init.lua
+++ nvim/lua/editor_config/init.lua
@@ -24,8 +24,6 @@ set.updatetime = 50
 
 set.modeline = true
 
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-
 local opts = {buffer = bufnr, remap = false}
 vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
 vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
@@ -37,7 +35,7 @@ vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts
 vim.keymap.set("n", "<leader>r", function() vim.lsp.buf.references() end, opts)
 vim.keymap.set("n", "<leader>lr", function() require('telescope.builtin').lsp_references() end, opts)
 vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
-vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+-- vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 
 vim.keymap.set("n", "<leader>e", function () vim.lsp.buf.format {
   filter = function(client) return client.name ~= "tsserver" end
