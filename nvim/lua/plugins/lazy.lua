e666416fb559d3243d281c3c6d362b4900f5c188
Peter Heilbo Ratgen <peter@pratgen.dk>
2025-04-11 14:36:52 +0200

    fix config

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -30,19 +30,10 @@ require('lazy').setup({
     'neovim/nvim-lspconfig',
     dependencies = { 'saghen/blink.cmp' },
   },
-  "folke/noice.nvim", {
-  event = "VeryLazy",
-  opts = {
-    -- add any options here
-  },
-  dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-    "MunifTanjim/nui.nvim",
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
-    "rcarriga/nvim-notify",
-    }
+  {
+    "folke/noice.nvim", 
+    event = "VeryLazy",
+    dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify", }
   },
   {
     'saghen/blink.cmp',
